37636123665f601f532b5dccdd71cc941ebf5334
Greyson Richey <greysonrichey@gmail.com>
2018-04-22 14:36:07 -0700

    Actually added stalagmite animation and company.

---
+++ Assets/Core/Scripts/Behaviors/RandomizeAnimation.cs
@@ -0,0 +1,15 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class RandomizeAnimation : MonoBehaviour {
+	public string AnimationName;
+
+	Animator _animator;
+
+	void Start () {
+		_animator = GetComponent<Animator>();
+
+		_animator.Play(AnimationName, -1, Random.value);
+	}
+}
